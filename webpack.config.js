28f6d155b96d3b7d43f77dbd5c1650541fc177f2
awkmia <vanomia@gmail.com>
2023-05-11 16:42:03 +0300

    1.1. FRONTEND: Setting webpack.config.js for output.

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const path = require("path");
 
 module.exports = {
     mode: 'development', //'production'
+    // entry: {ANY_NAME: path.resolve(__dirname, 'src', 'index.js')},
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, 'build')
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, 'build'),
+        clean: true,
     }
 }
