fe0b4055b87812805b60f46c8845e9433a0ea280
awkmia <vanomia@gmail.com>
2023-05-13 22:08:28 +0300

    1.10. FRONTEND: Edit in configuration Webpack absolute imports (https://webpack.js.org/concepts/module-resolution/; https://webpack.js.org/configuration/resolve/#resolvemodules)

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,28 +1,30 @@
+import { BuildOptions } from "./types/config";
 import webpack from "webpack";
-import {BuildOptions} from "./types/config";
-import {buildLoaders} from "./buildLoaders";
-import {buildResolvers} from "./buildResolvers";
-import {buildPlugins} from "./buildPlugins";
-import {buildDevServer} from "./buildDevServer";
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
 
-export function buildWebpackConfig(option : BuildOptions): webpack.Configuration {
-    const {paths, mode, isDev} = option
+export function buildWebpackConfig(
+    options: BuildOptions
+): webpack.Configuration {
+    const { paths, mode, isDev } = options;
 
     return {
         mode,
         // entry: {ANY_NAME: path.resolve(__dirname, 'src', 'index.ts')},
         entry: paths.entry,
-        module: {
-            rules: buildLoaders(option),
-        },
-        resolve: buildResolvers(),
         output: {
             filename: "[name].[contenthash].js",
             path: paths.build,
             clean: true,
         },
-        plugins: buildPlugins(option),
-        devtool: isDev ? 'inline-source-map' : undefined,
-        devServer: isDev ? buildDevServer(option) : undefined,
-    }
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(options),
+        devtool: isDev ? "inline-source-map" : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+    };
 }
