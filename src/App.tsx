1dbfade7c9e87c2a2c5e9eca100e57745980b579
awkmia <vanomia@gmail.com>
2023-05-13 19:18:48 +0300

    1.7. FRONTEND: Create Context, Provider, useTheme. (ТЕМЫ)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import React, {Suspense, useState} from 'react';
+import React, {Suspense} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import './styles/index.scss';
-
-export enum Theme {
-    LIGHT = "light",
-    DARK = "dark",
-}
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
-    const [theme, setTheme] = useState(Theme.LIGHT)
-
-    const toggleTheme = () => {
-        setTheme( theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
-    }
+    const {toggleTheme, theme} = useTheme()
 
     return (
         <div className={`app ${theme}`}>
