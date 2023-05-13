eb16241eec33b5b21a4a89c06d35f83f43c570f0
awkmia <vanomia@gmail.com>
2023-05-13 15:43:07 +0300

    1.6. FRONTEND: Added "lazy loaded" | "async chunk" | "code splitting" and use <Suspense /> (https://legacy.reactjs.org/docs/code-splitting.html)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './index.scss';
 import {Link, Route, Routes} from "react-router-dom";
-import MainPage from "./pages/MainPage/MainPage";
-import AboutPage from "./pages/AboutPage/AboutPage";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 
 const App = () => {
     return (
         <div className="app">
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-
+            <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPage />} />
-                    <Route path={'/'} element={<MainPage />} />
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
                 </Routes>
-
+            </Suspense>
         </div>
     );
 };
