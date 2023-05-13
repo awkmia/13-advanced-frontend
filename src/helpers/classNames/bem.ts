bd64511203a67a7db6ee1e48c2e3fc9999602659
awkmia <vanomia@gmail.com>
2023-05-13 20:40:04 +0300

    1.8. FRONTEND: Create helper for classNames.ts (Analog: https://github.com/JedWatson/classnames/blob/main/README.md)

---
+++ src/helpers/classNames/bem.ts
@@ -0,0 +1,36 @@
+type Modifiers = { [key: string]: boolean }
+type ExtraClasses = string[]
+
+function bem(block: string, modifiers: Modifiers, extraClasses?: ExtraClasses): string
+function bem(block: string, element: string, modifiers: Modifiers, extraClasses?: ExtraClasses): string
+function bem(
+    block: string, element: string | Modifiers, modifiers?: Modifiers | ExtraClasses, extraClasses?: ExtraClasses): string {
+    let baseClass = block
+    let classes = ''
+    if (typeof element === 'string') {
+        baseClass += `__${element}`
+    } else if (typeof element === 'object') {
+        extraClasses = modifiers as ExtraClasses
+        modifiers = element
+        element = null
+    }
+    classes += baseClass
+    if (modifiers) {
+        for (const [key, value] of Object.entries(modifiers)) {
+            if (value) {
+                classes += ` ${baseClass}--${key}`
+            }
+        }
+    }
+    if (extraClasses) {
+        extraClasses.forEach((extraClass) => {
+            classes += ` ${extraClass}`
+        })
+    }
+    return classes
+}
+
+export default bem
+
+//  Пример использования:
+// <div className={bem('app', {[theme]: true})}/>
