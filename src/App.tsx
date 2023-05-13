729e162264770880dbc78e1e4754fc736627b0fa
awkmia <vanomia@gmail.com>
2023-05-13 17:53:29 +0300

    1.7. FRONTEND: Added styles, global variables, reset and create toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React, { Suspense } from 'react';
-import './index.scss';
+import React, {Suspense, useState} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import './styles/index.scss';
+
+export enum Theme {
+    LIGHT = "light",
+    DARK = "dark",
+}
 
 const App = () => {
+    const [theme, setTheme] = useState(Theme.LIGHT)
+
+    const toggleTheme = () => {
+        setTheme( theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
+    }
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
