e9add254297a180fb7c3d716ec5d0e959643e645
awkmia <vanomia@gmail.com>
2023-05-14 22:37:32 +0300

    2.15. FRONTEND: Use chunk for translation RUS/EN

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {useTranslation} from "react-i18next";
 
 const AboutPage = () => {
+
+    // NameSpace нужно добавить public/locales/en/...
+    //Для подгрузки chunk в useTranslation() нужно первым аргументов передать название nameSpace, в данном случае 'about':
+    // const {t} = useTranslation('about');
     const {t} = useTranslation('about');
 
     return (
