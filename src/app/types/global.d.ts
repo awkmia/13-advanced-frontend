753dd31cfe883c3491f38ee9aae930a112f9afe9
awkmia <vanomia@gmail.com>
2023-05-14 17:06:14 +0300

    2.13. FRONTEND: Edit global.d.ts (import svg typescript: https://stackoverflow.com/questions/44717164/unable-to-import-svg-files-in-typescript; svg webpack typescript: https://github.com/gregberge/svgr/issues/546)

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -5,3 +5,18 @@ declare module '*.scss' {
     const classNames: IClassNames;
     export = classNames;
 }
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg' {
+    import React from 'react';
+
+    const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+    export default SVG;
+}
+
+/*declare module "*.svg" {
+    const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+    export default content;
+}*/
