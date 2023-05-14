0eb12b9dc82f067a260a9524d4a0c91408ed3618
awkmia <vanomia@gmail.com>
2023-05-14 22:59:02 +0300

    2.15. FRONTEND: Create component LangSwitcher for translation RUS/EN

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,18 +5,6 @@ import './styles/index.scss';
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
-import {useTranslation} from "react-i18next";
-
-function MyComponent() {
-    const { t, i18n } = useTranslation();
-
-    const toggle = () => {i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru")}
-
-    return <div>
-        <button onClick={toggle}>{t('Перевод')}</button>
-        {t('Тестовый пример')}
-    </div>
-}
 
 const App = () => {
     const { theme } = useTheme();
@@ -25,7 +13,6 @@ const App = () => {
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="Loading">
                 <Navbar />
-                <MyComponent />
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
