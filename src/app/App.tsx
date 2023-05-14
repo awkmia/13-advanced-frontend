dd1d01d5ecc44594cfa099592fd2da8d58d1457d
awkmia <vanomia@gmail.com>
2023-05-14 22:08:24 +0300

    2.15. FRONTEND: Hung TRANSLATION (doc: https://react.i18next.com/latest/using-with-hooks; i18next-http-backend: https://github.com/i18next/i18next-http-backend; "TypeError: i18n.changeLanguage is not a function": https://stackoverflow.com/questions/58176222/i18n-changelanguage-is-not-a-function)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import './styles/index.scss';
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
+import {useTranslation} from "react-i18next";
+
+function MyComponent() {
+    const { t, i18n } = useTranslation();
+
+    const toggle = () => {i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru")}
+
+    return <div>
+        <button onClick={toggle}>{t('Перевод')}</button>
+        {t('Тестовый пример')}
+    </div>
+}
 
 const App = () => {
     const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className="content-page">
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback="Loading">
+                <Navbar />
+                <MyComponent />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
