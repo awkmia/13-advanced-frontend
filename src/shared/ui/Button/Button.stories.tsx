ee5c4f55ec4d12d7ea2f1e86514f0c613d3851b9
awkmia <vanomia@gmail.com>
2023-05-17 14:55:51 +0300

    3.25. FRONTEND: REFACTORINGv2 like UlbiTV course. Create Button.stories.tsx.

---
+++ src/shared/ui/Button/Button.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'shared/Button',
+    component: Button,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    primary: true,
+    label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+    label: 'Button',
+};
