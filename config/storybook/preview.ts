643d1d1e13e1eb55eee39469bd145f17c041b2ac
awkmia <vanomia@gmail.com>
2023-05-18 00:06:33 +0300

    2.25. FRONTEND: NavBar.STORIES: Edit preview and add navbar

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,7 +1,8 @@
 import { addDecorator } from '@storybook/react';
-import { Theme } from 'app/providers/ThemeProvider';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '../../src/app/providers/ThemeProvider';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -15,3 +16,4 @@ export const parameters = {
 
 addDecorator(StyleDecorator);
 addDecorator(ThemeDecorator(Theme.LIGHT));
+addDecorator(RouterDecorator);
