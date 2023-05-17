edbc5559c745dbddc0cb2193362fe4f7291fed97
awkmia <vanomia@gmail.com>
2023-05-17 17:31:50 +0300

    3.25. FRONTEND: FIX v2: Storybook import css modules (https://storybook.js.org/addons/storybook-css-modules; https://stackoverflow.com/questions/59761361/storybook-ui-with-css-modules-and-less). Edit Button.

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+import { Button, ThemeButton } from './Button';
+import 'app/styles/index.scss';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -17,11 +18,17 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-    primary: true,
-    label: 'Button',
+    children: 'Text',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-    label: 'Button',
+export const Clear = Template.bind({});
+Clear.args = {
+    children: 'Text',
+    theme: ThemeButton.CLEAR,
+};
+
+export const Outline = Template.bind({});
+Outline.args = {
+    children: 'Text',
+    theme: ThemeButton.OUTLINE,
 };
