bc541fd7dcb3900c970c5f059f7ead29ef25266c
awkmia <vanomia@gmail.com>
2023-05-17 10:33:25 +0300

    3.24. FRONTEND: FIX: "Warning with useTranslation in Jest" (https://react.i18next.com/misc/testing).

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from '@testing-library/react';
+import { withTranslation } from 'react-i18next';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
     test('Test render', () => {
-        render(<Sidebar />);
+        const SidebarWithTranslation = withTranslation()(Sidebar);
+        render(<SidebarWithTranslation />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 });
