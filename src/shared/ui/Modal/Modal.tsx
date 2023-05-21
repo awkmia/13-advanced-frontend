33ef4a51312fbd04f81c72ecce0a30666fe3a6eb
awkmia <vanomia@gmail.com>
2023-05-21 16:46:28 +0300

    4.29. FRONTEND: MODAL: PORTAL: Create the portal (https://ru.react.js.org/docs/portals.html).

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -3,6 +3,7 @@ import React, {
 } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Modal.module.scss';
+import { Portal } from './Portal/Portal';
 
 interface ModalProps {
     className?: string,
@@ -61,12 +62,14 @@ export const Modal = (props: ModalProps) => {
     };
 
     return (
-        <div className={classNames(cls.Modal, mods, [className])}>
-            <div className={cls.overlay} onClick={closeHandler}>
-                <div className={cls.content} onClick={onContentClick}>
-                    {children}
+        <Portal>
+            <div className={classNames(cls.Modal, mods, [className])}>
+                <div className={cls.overlay} onClick={closeHandler}>
+                    <div className={cls.content} onClick={onContentClick}>
+                        {children}
+                    </div>
                 </div>
             </div>
-        </div>
+        </Portal>
     );
 };
