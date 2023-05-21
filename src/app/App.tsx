b233bdcec343fac0b271836530826f356b4b86dd
awkmia <vanomia@gmail.com>
2023-05-21 17:29:41 +0300

    4.29. FRONTEND: MODAL: Create auth_button in NavBar, and delete in App.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,18 @@
-import React, { Suspense, useState } from 'react';
+import React, { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Modal } from 'shared/ui/Modal/Modal';
 
 function App() {
     const { theme } = useTheme();
-
-    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
                 {/* eslint-disable-next-line i18next/no-literal-string */}
-                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-                    {/* eslint-disable-next-line i18next/no-literal-string */}
-                    {/* eslint-disable-next-line max-len */}
-                    i18n supporti18n supporti18n supporti18n supporti18n supporti18n supporti18n supporti18n
-                </Modal>
-                {/* eslint-disable-next-line i18next/no-literal-string */}
-                <button type="button" onClick={() => setIsOpen(true)}>Toggle</button>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Modal } from 'shared/ui/Modal/Modal';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -11,11 +12,25 @@ interface NavbarProps {
 export function Navbar({ className }: NavbarProps) {
     const { t } = useTranslation();
 
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
+
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                /
-            </div>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {/* eslint-disable-next-line */}
+                    {t('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.')}
+            </Modal>
         </div>
     );
 }
