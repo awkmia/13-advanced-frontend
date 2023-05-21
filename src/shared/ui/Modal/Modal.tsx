bab3be8e3da126ed7f428ae317aefb24ddfc2e89
awkmia <vanomia@gmail.com>
2023-05-21 16:53:32 +0300

    4.29. FRONTEND: MODAL: THEME: Terrible, don't do that: Hung the theme in to Modal.

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -2,6 +2,7 @@ import React, {
     ReactNode, useState, useRef, useEffect, useCallback,
 } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
 import cls from './Modal.module.scss';
 import { Portal } from './Portal/Portal';
 
@@ -24,6 +25,7 @@ export const Modal = (props: ModalProps) => {
 
     const [isClosing, setIsClosing] = useState(false);
     const timerRef = useRef<ReturnType<typeof setTimeout>>();
+    const { theme } = useTheme();
 
     const closeHandler = useCallback(() => {
         if (onClose) {
@@ -59,6 +61,7 @@ export const Modal = (props: ModalProps) => {
     const mods: Record<string, boolean> = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
+        [cls[theme]]: true,
     };
 
     return (
