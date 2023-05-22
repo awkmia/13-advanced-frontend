a36b56c2f739266d33776a21c16a2245d4cca263
awkmia <vanomia@gmail.com>
2023-05-22 13:58:51 +0300

    4.30. FRONTEND: REDUX TOOLKIT: Initialization (https://redux-toolkit.js.org/introduction/getting-started)

---
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+import { Provider } from 'react-redux';
+import { createReduxStore } from 'app/providers/StoreProvider/config/store';
+
+interface StoreProviderProps {
+    children?: ReactNode;
+}
+
+export const StoreProvider = (props: StoreProviderProps) => {
+    const {
+        children,
+    } = props;
+
+    const store = createReduxStore();
+
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    );
+};
