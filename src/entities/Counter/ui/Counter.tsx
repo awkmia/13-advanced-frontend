a793a22b895343c6ea8b31921c84d5599ceab3ea
awkmia <vanomia@gmail.com>
2023-05-22 15:00:33 +0300

    4.30. FRONTEND: REDUX TOOLKIT: Create Schema for Counter.

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,15 +1,25 @@
 import { Button } from 'shared/ui/Button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { counterActions } from '../model/slice/counterSlice';
 
 export const Counter = () => {
+    const dispatch = useDispatch();
+    const countValue = useSelector((state: StateSchema) => state.counter);
+
     const increment = () => {
+        dispatch(counterActions.increment);
     };
 
     const decrement = () => {
+        dispatch(counterActions.decrement);
     };
 
     return (
         <div>
-            <h1>value</h1>
+            <h1>
+                {countValue}
+            </h1>
             <Button
                 onClick={increment}
 
