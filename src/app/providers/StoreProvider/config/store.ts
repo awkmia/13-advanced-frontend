907c75a445522518186c562eed8d8425847a04d4
awkmia <vanomia@gmail.com>
2023-05-22 14:11:24 +0300

    4.30. FRONTEND: REDUX TOOLKIT: Create initialState for using store for Testing and Storybook.

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/StoreProvider';
 
-export function createReduxStore() {
+export function createReduxStore(initialState?: StateSchema) {
     return configureStore<StateSchema>({
-        reducer: {},
+        reducer: {
+        },
         devTools: __IS_DEV__,
+        preloadedState: initialState,
     });
 }
