d1d5fc02bb7847cba36126cb981e09a91af57ebe
awkmia <vanomia@gmail.com>
2023-05-22 21:50:13 +0300

    4.30. FRONTEND: REDUX TOOLKIT: TEST: Create test for Counter.

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,11 +1,13 @@
 import { Button } from 'shared/ui/Button/Button';
 import { useDispatch, useSelector } from 'react-redux';
-import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { useTranslation } from 'react-i18next';
 import { counterActions } from '../model/slice/counterSlice';
+import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
 
 export const Counter = () => {
     const dispatch = useDispatch();
-    const countValue = useSelector(getCounterValue);
+    const counterValue = useSelector(getCounterValue);
+    const { t } = useTranslation();
 
     const increment = () => {
         dispatch(counterActions.increment());
@@ -17,19 +19,18 @@ export const Counter = () => {
 
     return (
         <div>
-            <h1>
-                {countValue}
-            </h1>
+            <h1 data-testid="value-title">{counterValue}</h1>
             <Button
                 onClick={increment}
-
+                data-testid="increment-btn"
             >
-                increment
+                {t('increment')}
             </Button>
             <Button
+                data-testid="decrement-btn"
                 onClick={decrement}
             >
-                decrement
+                {t('decrement')}
             </Button>
         </div>
     );
