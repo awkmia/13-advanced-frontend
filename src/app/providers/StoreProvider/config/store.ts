bfb83cbeeb695f978990a9625bdb2e830b0c2316
awkmia <vanomia@gmail.com>
2023-05-22 14:02:32 +0300

    4.30. FRONTEND: REDUX TOOLKIT: Create Schema for use types

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
 
 export function createReduxStore() {
-    return configureStore({
+    return configureStore<StateSchema>({
         reducer: {},
         devTools: __IS_DEV__,
     });
