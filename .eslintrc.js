83e1b7c1fac4cb2ce7dd6f736805806efc4b38fe
awkmia <vanomia@gmail.com>
2023-05-24 00:46:49 +0300

    4.34. FRONTEND: REDUX TOOLKIT: Create Async Thunk (https://redux-toolkit.js.org/api/createAsyncThunk; https://redux-toolkit.js.org/api/createAsyncThunk#handling-thunk-errors).

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,7 +40,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'max-len': [2, { code: 100, ignoreComments: true, ignorePattern: '^.*function.*' }],
+        'max-len': [2, { code: 120, ignoreComments: true, ignorePattern: '^.*function.*' }],
         // 'i18next/no-literal-string': ['error', { markupOnly: true }],
         'i18next/no-literal-string': [
             'error', { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] }],
