eea6fe5b73837c0e583c9cf252f163cf4c59e77f
awkmia <vanomia@gmail.com>
2023-05-24 11:23:29 +0300

    4.34. FRONTEND: LOGIN: Create setAuthData.

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -1,5 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { UserSchema } from '../types/user';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { User, UserSchema } from '../types/user';
 
 const initialState: UserSchema = {};
 
@@ -7,7 +7,9 @@ export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-
+        setAuthData: (state, action : PayloadAction<User>) => {
+            state.authData = action.payload;
+        },
     },
 });
 
