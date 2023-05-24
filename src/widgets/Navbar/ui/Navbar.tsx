c282cd4b3fb220bc90b64f9c20b3f462c67a5cfd
awkmia <vanomia@gmail.com>
2023-05-24 12:02:19 +0300

    4.34. FRONTEND: LOGIN: Create logout.

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,18 +1,21 @@
 import React, { useCallback, useState } from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
+import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
-import { LoginModal } from 'features/AuthByUsername/ui/LoginModal/LoginModal';
+import { LoginModal } from 'features/AuthByUsername';
+import { getUserAuthData, userActions } from 'entities/User';
+import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
-    className?: string,
+    className?: string;
 }
 
-export function Navbar({ className }: NavbarProps) {
+export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
-
     const [isAuthModal, setIsAuthModal] = useState(false);
+    const authData = useSelector(getUserAuthData);
+    const dispatch = useDispatch();
 
     const onCloseModal = useCallback(() => {
         setIsAuthModal(false);
@@ -21,6 +24,24 @@ export function Navbar({ className }: NavbarProps) {
     const onShowModal = useCallback(() => {
         setIsAuthModal(true);
     }, []);
+
+    const onLogout = useCallback(() => {
+        dispatch(userActions.logout());
+    }, [dispatch]);
+
+    if (authData) {
+        return (
+            <div className={classNames(cls.Navbar, {}, [className])}>
+                <Button
+                    theme={ButtonTheme.CLEAR_INVERTED}
+                    className={cls.links}
+                    onClick={onLogout}
+                >
+                    {t('Выйти')}
+                </Button>
+            </div>
+        );
+    }
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
@@ -37,4 +58,4 @@ export function Navbar({ className }: NavbarProps) {
             />
         </div>
     );
-}
+};
