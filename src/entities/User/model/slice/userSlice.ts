17c26b26bf28af218df5c07c742c20bca2c5851b
awkmia <vanomia@gmail.com>
2023-05-24 11:28:38 +0300

    4.34. FRONTEND: LOGIN: Create initAuthData.

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 import { User, UserSchema } from '../types/user';
 
 const initialState: UserSchema = {};
@@ -9,6 +10,12 @@ export const userSlice = createSlice({
     reducers: {
         setAuthData: (state, action : PayloadAction<User>) => {
             state.authData = action.payload;
+        },
+        initAuthData: (state) => {
+            const user = localStorage.getItem(USER_LOCALSTORAGE_KEY);
+            if (user) {
+                state.authData = JSON.parse(user);
+            }
         },
     },
 });
