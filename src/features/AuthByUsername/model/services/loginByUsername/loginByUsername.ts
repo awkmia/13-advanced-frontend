f7da12718aa62c2dc52afc06fed6ae42df234d2c
awkmia <vanomia@gmail.com>
2023-05-24 14:41:52 +0300

    4.34. FRONTEND: STORYBOOK: Destroy TranslationDecorator (way out - 47:28).

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -2,12 +2,16 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { User, userActions } from 'entities/User';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
-import i18n from 'shared/config/i18n/i18n';
 
 interface LoginByUsernameProps {
     username: string,
     password: string,
 }
+
+// enum LoginErrors {
+//     INCORRECT_DATA = '',
+//     SERVER_ERROR = ''
+// }
 
 export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { rejectValue: string }>(
     'login/loginByUsername',
@@ -25,7 +29,7 @@ export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { re
             return response.data;
         } catch (e) {
             console.log('error: ', e);
-            return thunkAPI.rejectWithValue(i18n.t('Вы ввели неверный логин или пароль'));
+            return thunkAPI.rejectWithValue('error');
         }
     },
 );
