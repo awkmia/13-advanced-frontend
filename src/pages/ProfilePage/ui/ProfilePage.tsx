e7ce414dfd508ce0b8e333ddaa14d59d8d792a42
awkmia <vanomia@gmail.com>
2023-05-30 16:28:35 +0300

    5.37. FRONTEND: REDUX: Wrapped ProfilePAge in the DynamicModuleLoader for chunk loading state.

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,29 +1,26 @@
-import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { profileReducer } from 'entities/Profile';
+
+const reducers: ReducersList = {
+    profile: profileReducer,
+};
 
 interface ProfilePageProps {
-    className?: string,
+    className?: string;
 }
 
-const ProfilePage = (props: ProfilePageProps) => {
-    const {
-        className,
-    } = props;
-
+const ProfilePage = ({ className }: ProfilePageProps) => {
     const { t } = useTranslation();
 
-    const mods: Record<string, boolean> = {
-
-    };
-
     return (
-        <div className={classNames('', mods, [className])}>
-            {t('PROFILE PAGE')}
-        </div>
+        <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+            <div className={classNames('', {}, [className])}>
+                {t('PROFILE PAGE')}
+            </div>
+        </DynamicModuleLoader>
     );
 };
 
 export default ProfilePage;
-// Дефолтный экспорт нужен для того чтобы работала конструкция
-// в асинхронном чанке ProfilePage.async.tsx:
-// setTimeout(() => resolve(import('./ProfilePage')), 300);
