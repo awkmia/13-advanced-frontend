1a9a6095bf6a0f0ebefc742cd22ae68590e6bbee
awkmia <vanomia@gmail.com>
2023-05-31 11:11:00 +0300

    5.39. FRONTEND: REDUX: Edit DynamicModuleLoader.tsx.

--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -27,14 +27,14 @@ export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props) => {
 
     useEffect(() => {
         Object.entries(reducers).forEach(([name, reducer] : ReducersListEntry) => {
-            store.reducerManager.add(name, reducer);
-            dispatch({ type: '@INIT loginForm reducer' });
+            store.reducerManager.add(name as StateSchemaKey, reducer);
+            dispatch({ type: `@INIT ${name} reducer` });
         });
 
         return () => {
             if (removeAfterUnmount) {
                 Object.entries(reducers).forEach(([name, reducer]: ReducersListEntry) => {
-                    store.reducerManager.remove(name);
+                    store.reducerManager.remove(name as StateSchemaKey);
                     dispatch({ type: `@DESTROY ${name} reducer` });
                 });
             }
