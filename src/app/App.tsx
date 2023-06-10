b0c063043b77852e226fa4b1d1e4dd9091741d33
awkmia <vanomia@gmail.com>
2023-06-10 23:57:11 +0300

    6.47. FRONTEND: COMPONENT: Create protected rotes. Edit: App.tsx, AppRouter.tsx, RequireAuth.tsx.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,13 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { userActions } from 'entities/User';
-import { useDispatch } from 'react-redux';
+import { getUserInited, userActions } from 'entities/User';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
     const { theme } = useTheme();
     const dispatch = useDispatch();
+    const inited = useSelector(getUserInited);
 
     useEffect(() => {
         dispatch(userActions.initAuthData());
@@ -22,7 +23,7 @@ function App() {
                 {/* eslint-disable-next-line i18next/no-literal-string */}
                 <div className="content-page">
                     <Sidebar />
-                    <AppRouter />
+                    {inited && <AppRouter />}
                 </div>
             </Suspense>
         </div>
