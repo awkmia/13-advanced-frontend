8076f0e50ba7de5d79247982f35e06a2bd5292b5
awkmia <vanomia@gmail.com>
2023-06-13 23:31:12 +0300

    6.50. FRONTEND: COMPONENT: Completed Article_BlockComponent: ArticleCodeBlockComponent.tsx, ArticleImageBlockComponent.tsx, ArticleImageBlockComponent.module.scss, ArticleTextBlockComponent.tsx, ArticleTextBlockComponent.module.scss.

--- src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.tsx
+++ src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.tsx
@@ -1,24 +1,27 @@
+import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
+import { Text } from 'shared/ui/Text/Text';
 import cls from './ArticleTextBlockComponent.module.scss';
+import { ArticleTextBlock } from '../../model/types/article';
 
 interface ArticleTextBlockComponentProps {
-    className?: string,
+    className?: string;
+    block: ArticleTextBlock;
 }
 
 export const ArticleTextBlockComponent = memo((props: ArticleTextBlockComponentProps) => {
-    const {
-        className,
-    } = props;
-
+    const { className, block } = props;
     const { t } = useTranslation();
 
-    const mods: Record<string, boolean> = {};
-
     return (
-        <div className={classNames(cls.articleTextBlockComponent, mods, [className])}>
-            ArticleTextBlockComponent
+        <div className={classNames(cls.ArticleTextBlockComponent, {}, [className])}>
+            {block.title && (
+                <Text title={block.title} className={cls.title} />
+            )}
+            {block.paragraphs.map((paragraph, index) => (
+                <Text key={paragraph} text={paragraph} className={cls.paragraph} />
+            ))}
         </div>
     );
 });
