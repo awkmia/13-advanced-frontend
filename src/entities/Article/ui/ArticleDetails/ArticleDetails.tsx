937cd933f42eea8f9a67efdda7354d7237a1659f
awkmia <vanomia@gmail.com>
2023-06-13 20:07:09 +0300

    6.49. FRONTEND: COMPONENT: Create selectors (articleDetails.ts), scss (ArticleDetails.module.scss ) for ArticleDetails. Add them in the ArticleDetails.tsx.

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -1,9 +1,16 @@
 import { useTranslation } from 'react-i18next';
 import { memo, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { fetchArticleById } from 'entities/Article/model/services/fetchArticleById/fetchArticleById';
+import { Text, TextAlign } from 'shared/ui/Text/Text';
+import {
+    getArticleDetailsData,
+    getArticleDetailsError,
+    getArticleDetailsIsLoading,
+} from '../../model/selectors/articleDetails';
 import cls from './ArticleDetails.module.scss';
 import { articleDetailsReducer } from '../../model/slice/articleDetailsSlice';
 
@@ -21,20 +28,37 @@ export const ArticleDetails = memo((props: ArticleDetailsProps) => {
         id,
         className,
     } = props;
+    const { t } = useTranslation();
     const dispatch = useAppDispatch();
+    const isLoading = useSelector(getArticleDetailsIsLoading);
+    const article = useSelector(getArticleDetailsData);
+    const error = useSelector(getArticleDetailsError);
 
     useEffect(() => {
         dispatch(fetchArticleById(id));
     }, [dispatch, id]);
 
-    const { t } = useTranslation();
-
     const mods: Record<string, boolean> = {};
+
+    let content;
+
+    if (isLoading) {
+        content = (<div>Lading...</div>);
+    } else if (error) {
+        content = (
+            <Text
+                align={TextAlign.CENTER}
+                title={t('Произошла ошибка при загрузке статьи.')}
+            />
+        );
+    } else {
+        content = (<div>ARTICLE DETAILS COMP</div>);
+    }
 
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-            <div className={classNames(cls.articledetails, mods, [className])}>
-                ARTICLE DETAILS COMP
+            <div className={classNames(cls.articleDetails, mods, [className])}>
+                {content}
             </div>
         </DynamicModuleLoader>
     );
