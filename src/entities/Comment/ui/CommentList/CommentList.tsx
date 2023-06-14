c57a6a9b5d3043f4d50be3124da83a620baddc0d
awkmia <vanomia@gmail.com>
2023-06-14 20:50:49 +0300

    6.51. FRONTEND: COMPONENT: Create CommentList, CommentCard, types and public API. Edits: CommentList.tsx, CommentList.module.scss, CommentList.stories.tsx, CommentCard.tsx, CommentCard.module.scss, CommentCard.stories.tsx, index.ts and translation.json.

---
+++ src/entities/Comment/ui/CommentList/CommentList.tsx
@@ -0,0 +1,41 @@
+import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Text } from 'shared/ui/Text/Text';
+import { CommentCard } from 'entities/Comment/ui/CommentCard/CommentCard';
+import { Comment } from '../../model/types/comment';
+import cls from './CommentList.module.scss';
+
+interface CommentListProps {
+    className?: string,
+    comments?: Comment[];
+    isLoading?: boolean;
+}
+
+export const CommentList = memo((props: CommentListProps) => {
+    const {
+        className,
+        comments,
+        isLoading,
+    } = props;
+
+    const { t } = useTranslation();
+
+    const mods: Record<string, boolean> = {};
+
+    return (
+        <div className={classNames(cls.CommentList, mods, [className])}>
+            {
+                comments?.length
+                    ? comments.map((comment) => (
+                        <CommentCard
+                            isLoading
+                            className={cls.comment}
+                            comment={comment}
+                        />
+                    ))
+                    : <Text text={t('Комментарии отсутствуют.')} />
+            }
+        </div>
+    );
+});
