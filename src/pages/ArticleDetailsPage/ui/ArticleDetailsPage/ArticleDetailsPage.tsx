fbb0df655d25686c527cc2b215494813607e2934
awkmia <vanomia@gmail.com>
2023-06-14 20:52:24 +0300

    6.51. FRONTEND: COMPONENT: Add CommentList in the ArticleDetailsPage.tsx. Add properties avatar to the user. Edits: ArticleDetailsPage.module.scss, db.json.

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -2,7 +2,9 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { ArticleDetails } from 'entities/Article';
+import { Text } from 'shared/ui/Text/Text';
 import { useParams } from 'react-router-dom';
+import { CommentList } from 'entities/Comment';
 import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
@@ -25,6 +27,22 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     return (
         <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
             <ArticleDetails id={id} />
+            <Text className={cls.commentTitle} title={t('Комментарии')} />
+            <CommentList
+                isLoading
+                comments={[
+                    {
+                        id: '1',
+                        text: 'Comment 1',
+                        user: { id: '1', username: 'awkmia', avatar: 'https://xakep.ru/wp-content/uploads/2018/05/171485/KuroiSH-hacker.jpg' },
+                    },
+                    {
+                        id: '2',
+                        text: 'Comment 2',
+                        user: { id: '2', username: 'ulbi', avatar: 'https://xakep.ru/wp-content/uploads/2018/05/171485/KuroiSH-hacker.jpg' },
+                    },
+                ]}
+            />
         </div>
     );
 };
