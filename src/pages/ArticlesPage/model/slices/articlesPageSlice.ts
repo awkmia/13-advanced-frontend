02fcaa21601559953e84f3bdee65bb71159be73d
awkmia <vanomia@gmail.com>
2023-06-21 15:07:33 +0300

    6.55. FRONTEND: REDUX: Add extraReducers in the articlesPageSlice.ts.

--- src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
+++ src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
@@ -1,7 +1,8 @@
 import { createEntityAdapter, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/StoreProvider';
 import { Article, ArticleView } from 'entities/Article';
-import { ArticlesPageSchema } from 'pages/ArticlesPage';
+import { ArticlesPageSchema } from '../types/articlesPageSchema';
+import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
 
 const articlesAdapter = createEntityAdapter<Article>({
     selectId: (article) => article.id,
@@ -24,6 +25,24 @@ const articlesPageSlice = createSlice({
         setView: (state, action: PayloadAction<ArticleView>) => {
             state.view = action.payload;
         },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchArticlesList.pending, (state) => {
+                state.error = undefined;
+                state.isLoading = true;
+            })
+            .addCase(fetchArticlesList.fulfilled, (
+                state,
+                action: PayloadAction<Article[]>,
+            ) => {
+                state.isLoading = false;
+                articlesAdapter.setAll(state, action.payload);
+            })
+            .addCase(fetchArticlesList.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
     },
 });
 
