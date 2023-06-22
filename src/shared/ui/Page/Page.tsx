1530916e83a4e52b1bafb20a178e608bc43cc762
awkmia <vanomia@gmail.com>
2023-06-22 23:47:54 +0300

    6.56. FRONTEND: COMPONENT: Create infinity scroll. Edit: Page.tsx, useInfiniteScroll.ts. (https://developer.mozilla.org/ru/docs/Web/API/Intersection_Observer_API)

--- src/shared/ui/Page/Page.tsx
+++ src/shared/ui/Page/Page.tsx
@@ -1,5 +1,8 @@
-import { ReactNode } from 'react';
+import {
+    memo, MutableRefObject, ReactNode, useRef,
+} from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useInfiniteScroll } from 'shared/lib/hooks/useInfiniteScroll/useInfiniteScroll';
 import cls from './Page.module.scss';
 
 interface PageProps {
@@ -7,17 +10,24 @@ interface PageProps {
     children?: ReactNode
 }
 
-export const Page = (props: PageProps) => {
-    const {
-        children,
-        className,
-    } = props;
+export const Page = memo((props: PageProps) => {
+    const { className, children } = props;
+    const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
+    const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
 
-    const mods: Record<string, boolean> = {};
+    useInfiniteScroll({
+        triggerRef,
+        wrapperRef,
+        callback: () => console.log('callback'),
+    });
 
     return (
-        <section className={classNames(cls.Page, mods, [className])}>
+        <section
+            ref={wrapperRef}
+            className={classNames(cls.Page, {}, [className])}
+        >
             {children}
+            <div ref={triggerRef} />
         </section>
     );
-};
+});
