f20b819bea607691dbff33dfcee7ec0556dec0cd
awkmia <vanomia@gmail.com>
2023-06-23 22:39:00 +0300

    6.56. FRONTEND: HOOK: Debugger in the useInfiniteScroll.ts.

--- src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -10,12 +10,9 @@ export function useInfiniteScroll({ callback, wrapperRef, triggerRef }: UseInfin
     const observer = useRef<IntersectionObserver | null>(null);
 
     useEffect(() => {
-        const wrapperElement = wrapperRef.current;
-        const triggerElement = triggerRef.current;
-
         if (callback) {
             const options = {
-                root: wrapperElement,
+                root: wrapperRef.current,
                 rootMargin: '0px',
                 threshold: 1.0,
             };
@@ -26,13 +23,14 @@ export function useInfiniteScroll({ callback, wrapperRef, triggerRef }: UseInfin
                 }
             }, options);
 
-            observer.current.observe(triggerElement);
+            observer.current.observe(triggerRef.current);
         }
 
         return () => {
-            if (observer.current && triggerElement) {
+            if (observer.current) {
+                debugger;
                 // eslint-disable-next-line react-hooks/exhaustive-deps
-                observer.current.unobserve(triggerElement);
+                observer.current.unobserve(triggerRef.current);
             }
         };
     }, [callback, triggerRef, wrapperRef]);
