cbaec2c30a1b4d5e239d6e2397f5b89767ddbcfb
awkmia <vanomia@gmail.com>
2023-06-25 10:44:32 +0300

    9.58. FRONTEND: COMPONENT: Fix: useThrottle.ts remove memory leak.

--- src/shared/lib/hooks/useThrottle/useThrottle.ts
+++ src/shared/lib/hooks/useThrottle/useThrottle.ts
@@ -1,16 +1,23 @@
-import { useCallback, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 export function useThrottle(callback: (...args: any[]) => void, delay: number) {
     const throttleRef = useRef(false);
+    const timeoutRef = useRef<any>(null);
 
-    return useCallback((...args: any[]) => {
+    const throttledCallback = useCallback((...args: any[]) => {
         if (!throttleRef.current) {
             callback(...args);
             throttleRef.current = true;
 
-            setTimeout(() => {
+            timeoutRef.current = setTimeout(() => {
                 throttleRef.current = false;
             }, delay);
         }
     }, [callback, delay]);
+
+    useEffect(() => () => {
+        clearTimeout(timeoutRef.current);
+    }, []);
+
+    return throttledCallback;
 }
