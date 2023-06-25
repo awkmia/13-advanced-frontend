84928e1e52e82d8f0e0c9b9b5b465cfeb4487fd4
awkmia <vanomia@gmail.com>
2023-06-25 20:53:36 +0300

    9.59. FRONTEND: COMPONENT: Create ArticleSortSelector.tsx, ArticleSortSelector.stories.tsx, ArticleSortSelector.module.scss and add their in the ArticlesPageFilters.tsx. Create new selector for ArticlesPageFilters.tsx. Edit public API: index.ts. Edit: translation.json.

--- src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
@@ -2,7 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo, useCallback, useMemo } from 'react';
 import {
-    ArticleSortField, ArticleView, ArticleViewSelector,
+    ArticleSortField, ArticleSortSelector, ArticleView, ArticleViewSelector,
 } from 'entities/Article';
 import { useSelector } from 'react-redux';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
@@ -15,6 +15,7 @@ import { ArticleType } from 'entities/Article/model/types/article';
 import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
 import cls from './ArticlesPageFilters.module.scss';
 import {
+    getArticlesPageOrder, getArticlesPageSearch, getArticlesPageSort, getArticlesPageType,
     getArticlesPageView,
 } from '../../model/selectors/articlesPageSelectors';
 import { articlesPageActions } from '../../model/slices/articlesPageSlice';
@@ -29,15 +30,34 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
     const mods: Record<string, boolean> = {};
     const dispatch = useAppDispatch();
     const view = useSelector(getArticlesPageView);
+    const sort = useSelector(getArticlesPageSort);
+    const order = useSelector(getArticlesPageOrder);
+    const search = useSelector(getArticlesPageSearch);
+    const type = useSelector(getArticlesPageType);
 
     const onChangeView = useCallback((view: ArticleView) => {
         dispatch(articlesPageActions.setView(view));
     }, [dispatch]);
 
+    const onChangeSort = useCallback((newSort: ArticleSortField) => {
+        dispatch(articlesPageActions.setSort(newSort));
+        dispatch(articlesPageActions.setPage(1));
+    }, [dispatch]);
+
+    const onChangeOrder = useCallback((newOrder: SortOrder) => {
+        dispatch(articlesPageActions.setOrder(newOrder));
+        dispatch(articlesPageActions.setPage(1));
+    }, [dispatch]);
+
     return (
         <div className={classNames(cls.ArticlesPageFilters, {}, [className])}>
             <div className={cls.sortWrapper}>
-                <Select label={t('Сортировать ПО')} />
+                <ArticleSortSelector
+                    sort={sort}
+                    order={order}
+                    onChangeOrder={onChangeOrder}
+                    onChangeSort={onChangeSort}
+                />
                 <ArticleViewSelector
                     view={view}
                     onViewClick={onChangeView}
