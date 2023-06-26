4ef460a7bc6949b655b6a33f17553fc68e438c10
awkmia <vanomia@gmail.com>
2023-06-26 16:44:09 +0300

    9.60. FRONTEND: LINTER: Fix rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,7 @@ module.exports = {
         'max-len': [2, { code: 125, ignoreComments: true, ignorePattern: '^.*function.*' }],
         // 'i18next/no-literal-string': ['error', { markupOnly: true }],
         'i18next/no-literal-string': [
-            'error', { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] }],
+            'error', { markupOnly: true, ignoreAttribute: ['data-testid', 'to', 'target'] }],
         // 'i18next/no-literal-string': 'off',
         'no-console': 'off',
         // 'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
