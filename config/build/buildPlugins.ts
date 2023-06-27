2c8ff2a84fc27cea0820cd23f31d3c58f19ad4dc
awkmia <vanomia@gmail.com>
2023-06-27 23:00:37 +0300

    9.62. FRONTEND: NPM: Install CopyWebpackPlugin: npm install copy-webpack-plugin@10.2.4 --save-dev (https://webpack.js.org/plugins/copy-webpack-plugin/). Edit webpack.config.ts, config.ts, buildPlugins.ts because of Translation.

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import CopyPlugin from 'copy-webpack-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
@@ -23,6 +24,11 @@ export function buildPlugins({
             __PROJECT__: JSON.stringify(project),
         }),
         // new webpack.HotModuleReplacementPlugin(),
+        new CopyPlugin({
+            patterns: [
+                { from: paths.locales, to: paths.buildLocales },
+            ],
+        }),
 
     ];
 
