24e8eae50acf391d2951715de41bbd22cde5ebe1
awkmia <vanomia@gmail.com>
2023-06-29 17:31:00 +0300

    10.67. FRONTEND: ESLINT: Add new rule in the .eslintrc.js.

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,7 +47,7 @@ module.exports = {
             'error', {
                 markupOnly: true,
                 ignoreAttribute: [
-                    'role', 'data-testid', 'to', 'target', 'justify', 'align', 'direction', 'gap',
+                    'as', 'role', 'data-testid', 'to', 'target', 'justify', 'align', 'direction', 'gap',
                 ],
             }],
         // 'i18next/no-literal-string': 'off',
