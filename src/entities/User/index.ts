1b9adff7ca7c4326c9a28dfe5098ae2245cf595f
awkmia <vanomia@gmail.com>
2023-07-01 19:50:50 +0300

    11.73. FRONTEND: TYPE: Add roles to the user, db.json. And use in the selectors: roleSelectors.ts and public API: index.ts.

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,9 +1,12 @@
 export {
     getUserAuthData,
 } from './model/selectors/getUserAuthData/getUserAuthData';
+
 export {
     getUserInited,
 } from './model/selectors/getUserInited/getUserInited';
+
+export { isUserAdmin, isUserManager, getUserRoles } from './model/selectors/roleSelectors';
 
 export {
     userReducer,
@@ -12,4 +15,5 @@ export {
 export {
     UserSchema,
     User,
+    UserRole,
 } from './model/types/user';
