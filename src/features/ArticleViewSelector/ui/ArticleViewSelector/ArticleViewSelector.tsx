ab50f2ec77b147cc419e695182823eeb5db9219c
awkmia <vanomia@gmail.com>
2023-07-20 00:06:46 +0300

    13.103. FRONTEND: REFACTORING: Refactoring and moving entities ArticleTypeTabs.tsx, ArticleTypeTabs.stories.tsx, ArticleViewSelector.tsx, ArticleViewSelector.module.scss, ArticleViewSelector.stories.tsx, ArticleSortSelector.tsx, ArticleSortSelector.module.scss, ArticleSortSelector.stories.tsx. Edit public API index.ts and ArticlesPageFilters.tsx.

--- src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
+++ src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -4,8 +4,8 @@ import ListIcon from '@/shared/assets/icons/list-24-24.svg';
 import TiledIcon from '@/shared/assets/icons/tiled-24-24.svg';
 import { Icon } from '@/shared/ui/Icon';
 import { Button, ButtonTheme } from '@/shared/ui/Button';
-import { ArticleView } from '../../model/consts/articleConsts';
 import cls from './ArticleViewSelector.module.scss';
+import { ArticleView } from '@/entities/Article';
 
 interface ArticleViewSelectorProps {
     className?: string;
@@ -33,11 +33,11 @@ export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
 
     return (
         <div className={classNames(cls.ArticleViewSelector, {}, [className])}>
-            {viewTypes.map((viewType, index) => (
+            {viewTypes.map((viewType) => (
                 <Button
+                    key={viewType.view}
                     theme={ButtonTheme.CLEAR}
                     onClick={onClick(viewType.view)}
-                    key={viewType.view}
                 >
                     <Icon
                         Svg={viewType.icon}
