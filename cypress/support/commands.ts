5103d32624b73e3c6d968e3d0d23b3205351baee
awkmia <vanomia@gmail.com>
2023-08-02 14:19:37 +0300

    14.113. FRONTEND: E2E: Create command for login (https://docs.cypress.io/guides/end-to-end-testing/auth0-authentication) commands.ts.

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,11 +1,26 @@
-Cypress.Commands.add('login', (email, password) => {});
+import {USER_LOCALSTORAGE_KEY} from "@/shared/const/localstorage";
+
+Cypress.Commands.add('login', (username: string = 'testuser', password: string = '123') => {
+    cy.log(`Logging in as ${username}`)
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:8000/login',
+        body: {
+            grant_type: 'password',
+            username,
+            password,
+        },
+    }).then(({body}) => {
+        window.localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(body))
+    });
+});
 
 declare global {
-  namespace Cypress {
-    interface Chainable {
-      login(email: string, password: string): Chainable<void>
+    namespace Cypress {
+        interface Chainable {
+            login(username?: string, password?: string): Chainable<void>
+        }
     }
-  }
 }
 
 export {};
