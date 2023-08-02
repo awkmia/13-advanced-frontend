3edc0eaa3f829a9d0512dd8e59a48ec33534637d
awkmia <vanomia@gmail.com>
2023-08-02 15:34:36 +0300

    14.114. FRONTEND: E2E: Create new e2e test profile-edit.cy.ts. Edit: commands.ts and login.ts.

---
+++ cypress/e2e/profile/profile-edit.cy.ts
@@ -0,0 +1,10 @@
+describe('Пользователь заходит на страницу профиля', () => {
+    beforeEach(() => {
+        cy.visit('');
+        cy.login().then((data) => {
+            cy.visit(`profile/${data.id}`);
+        });
+    });
+    it('И редактирует его', () => {
+    });
+});
