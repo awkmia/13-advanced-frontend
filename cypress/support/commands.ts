12d1075d17e1bb2f3c30ca595a5bc9f7283cdb97
awkmia <vanomia@gmail.com>
2023-08-02 14:21:44 +0300

    14.113. FRONTEND: E2E: Decomposition command for login commands.ts, login.ts.

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,24 +1,11 @@
-import {USER_LOCALSTORAGE_KEY} from "@/shared/const/localstorage";
+import { login } from './commands/login';
 
-Cypress.Commands.add('login', (username: string = 'testuser', password: string = '123') => {
-    cy.log(`Logging in as ${username}`)
-    cy.request({
-        method: 'POST',
-        url: 'http://localhost:8000/login',
-        body: {
-            grant_type: 'password',
-            username,
-            password,
-        },
-    }).then(({body}) => {
-        window.localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(body))
-    });
-});
+Cypress.Commands.add('login', login);
 
 declare global {
     namespace Cypress {
         interface Chainable {
-            login(username?: string, password?: string): Chainable<void>
+            login(email?: string, password?: string): Chainable<void>
         }
     }
 }
