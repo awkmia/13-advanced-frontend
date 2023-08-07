4b45301b34622fe2f4208827201592f1a98ff05f
awkmia <vanomia@gmail.com>
2023-08-07 14:49:15 +0300

    14.116. FRONTEND: E2E: Add Theme provider in the componentRender.tsx. Create isolated component with cypress EditableProfileCard.cy.tsx, component.ts, component-index.html.

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -5,31 +5,42 @@ import { MemoryRouter } from 'react-router-dom';
 import { ReducersMapObject } from '@reduxjs/toolkit';
 import i18nForTests from '@/shared/config/i18n/i18nForTests';
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
+import { Theme } from '@/shared/const/theme';
+// eslint-disable-next-line awkmia-plugin/layer-imports
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
+// eslint-disable-next-line awkmia-plugin/layer-imports
+import '@/app/styles/index.scss';
 
 export interface componentRenderOptions {
     route?: string;
     initialState?: DeepPartial<StateSchema>;
-    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
+    theme?: Theme;
 }
 
 interface TestProviderProps {
-    children?: ReactNode,
-    options?: componentRenderOptions
+    children: ReactNode;
+    options?: componentRenderOptions;
 }
 
-export function TestProvider(props : TestProviderProps) {
+export function TestProvider(props: TestProviderProps) {
     const { children, options = {} } = props;
     const {
         route = '/',
         initialState,
         asyncReducers,
+        theme = Theme.LIGHT,
     } = options;
 
     return (
         <MemoryRouter initialEntries={[route]}>
             <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
                 <I18nextProvider i18n={i18nForTests}>
-                    {children}
+                    <ThemeProvider initialTheme={theme}>
+                        <div className={`app ${theme}`}>
+                            {children}
+                        </div>
+                    </ThemeProvider>
                 </I18nextProvider>
             </StoreProvider>
         </MemoryRouter>
