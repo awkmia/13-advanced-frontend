3114d99f63b9f37debd5a79b5bbc876ec6fa99ee
awkmia <vanomia@gmail.com>
2023-08-07 14:30:12 +0300

    14.116. FRONTEND: E2E: Prepare webpack.config.ts. Add tsx template in the tsconfig.json. Component testing editing cypress.config.ts. Refactoring componentRender.tsx. Create stub and fixture profile.json.

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -12,20 +12,30 @@ export interface componentRenderOptions {
     asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
 }
 
-export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
+interface TestProviderProps {
+    children?: ReactNode,
+    options?: componentRenderOptions
+}
+
+export function TestProvider(props : TestProviderProps) {
+    const { children, options = {} } = props;
     const {
         route = '/',
         initialState,
         asyncReducers,
     } = options;
 
-    return render(
+    return (
         <MemoryRouter initialEntries={[route]}>
             <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
                 <I18nextProvider i18n={i18nForTests}>
-                    {component}
+                    {children}
                 </I18nextProvider>
             </StoreProvider>
-        </MemoryRouter>,
+        </MemoryRouter>
     );
+}
+
+export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
+    return render(<TestProvider options={options}>{component}</TestProvider>);
 }
