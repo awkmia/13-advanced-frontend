3d4b9d3bbda8212acda9c4e5bcdb503006077354
awkmia <vanomia@gmail.com>
2023-08-11 15:53:52 +0300

    15.128. FRONTEND: COMPONENT: Init auth data from server and sawing localStorage. Edit asyncThunk request in the loginByUsername.ts. Edit ThemeProvider.tsx.  Edit App.tsx. Edit userSlice.ts Create services initAuthData.ts. Edit userApi.ts. Edit public API index.ts.

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,11 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { User, userActions } from '@/entities/User';
-import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
 import { ThunkConfig } from '@/app/providers/StoreProvider';
 
 interface LoginByUsernameProps {
-    username: string,
-    password: string,
+    username: string;
+    password: string;
 }
 
 // enum LoginErrors {
@@ -17,23 +16,19 @@ export const loginByUsername = createAsyncThunk<
     User,
     LoginByUsernameProps,
     ThunkConfig<string>
-    >(
-        'login/loginByUsername',
-        async (authData, thunkApi) => {
-            const { extra, dispatch, rejectWithValue } = thunkApi;
-            try {
-                const response = await extra.api.post<User>('/login', authData);
+>('login/loginByUsername', async (authData, thunkApi) => {
+    const { extra, dispatch, rejectWithValue } = thunkApi;
+    try {
+        const response = await extra.api.post<User>('/login', authData);
 
-                if (!response.data) {
-                    throw new Error();
-                }
+        if (!response.data) {
+            throw new Error();
+        }
 
-                localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
-                dispatch(userActions.setAuthData(response.data));
-                return response.data;
-            } catch (e) {
-                console.log('error: ', e);
-                return rejectWithValue('error');
-            }
-        },
-    );
+        dispatch(userActions.setAuthData(response.data));
+        return response.data;
+    } catch (e) {
+        console.log('error: ', e);
+        return rejectWithValue('error');
+    }
+});
