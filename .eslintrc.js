674d4035c56cb99c74bc8c47a5292667dde34277
awkmia <vanomia@gmail.com>
2023-08-11 19:45:03 +0300

    15.130. FRONTEND: ESLINT: Add new ignore attribute in the .eslintrc.js.

--- .eslintrc.js
+++ .eslintrc.js
@@ -72,6 +72,7 @@ module.exports = {
                     'border',
                     'direction',
                     'gap',
+                    'feature',
                 ],
             },
         ],
