4aaeb8eab86ba5aa9daa32ef3ad1e7877a34445e
awkmia <vanomia@gmail.com>
2023-09-08 10:19:21 +0300

    16.134. FRONTEND: REDESIGN: Edit redesign Button.tsx, Button.module.scss, Button.stories.tsx, Button.test.tsx.

--- src/shared/ui/redesigned/Button/Button.tsx
+++ src/shared/ui/redesigned/Button/Button.tsx
@@ -2,27 +2,16 @@ import { ButtonHTMLAttributes, memo, ReactNode } from 'react';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Button.module.scss';
 
-export enum ButtonTheme {
-    CLEAR = 'clear',
-    CLEAR_INVERTED = 'clearInverted',
-    OUTLINE = 'outline',
-    OUTLINE_RED = 'outline_red',
-    BACKGROUND = 'background',
-    BACKGROUND_INVERTED = 'backgroundInverted',
-}
+export type ButtonVariant = 'clear' | 'outline';
 
-export enum ButtonSize {
-    M = 'size_m',
-    L = 'size_l',
-    XL = 'size_xl',
-}
+export type ButtonSize = 'm' | 'l' | 'xl';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string;
     /**
      * Тема кнопки. Отвечает за визуал (в рамке, без стилей, противоположный теме приложения цвет и тд)
      */
-    theme?: ButtonTheme;
+    variant?: ButtonVariant;
     /**
      * Флаг, делающий кнопку квадратной
      */
@@ -49,9 +38,9 @@ export const Button = memo((props: ButtonProps) => {
     const {
         className,
         children,
-        theme = ButtonTheme.OUTLINE,
+        variant = 'outline',
         square,
-        size = ButtonSize.M,
+        size = 'm',
         disabled,
         fullWidth,
         ...otherProps
@@ -68,7 +57,7 @@ export const Button = memo((props: ButtonProps) => {
             type="button"
             className={classNames(cls.Button, mods, [
                 className,
-                cls[theme],
+                cls[variant],
                 cls[size],
             ])}
             disabled={disabled}
