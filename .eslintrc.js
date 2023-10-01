35c4d1f5dcb177488c97696e142384523735a71f
awkmia <vanomia@gmail.com>
2023-10-01 16:03:54 +0300

    16.141. FRONTEND: ESLINT: Add "wrap" in to ignore list .eslintrc.js.

--- .eslintrc.js
+++ .eslintrc.js
@@ -76,6 +76,7 @@ module.exports = {
                     'color',
                     'variant',
                     'size',
+                    'wrap',
                 ],
             },
         ],
