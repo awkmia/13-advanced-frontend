b29551106beef6e98c22e1d3b7373fd4748856c4
awkmia <vanomia@gmail.com>
2023-10-08 21:58:10 +0300

    16.151. FRONTEND: REDESIGNED: Create two hooks for scroll toolbar (and anything) useAppToolbar.tsx and useRouteChange.tsx, edit for it router.ts. And use his in the App.tsx.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,12 +11,13 @@ import { ToggleFeatures } from '@/shared/lib/features';
 import { MainLayout } from '@/shared/layouts/MainLayout';
 import { AppLoaderLayout } from '@/shared/layouts/AppLoaderLayout';
 import { PageLoader } from '@/widgets/PageLoader';
-import { ScrollToolbar } from '@/widgets/ScrollToolbar';
+import { useAppToolbar } from './lib/useAppToolbar';
 
 function App() {
     const { theme } = useTheme();
     const dispatch = useAppDispatch();
     const inited = useSelector(getUserInited);
+    const toolbar = useAppToolbar();
 
     useEffect(() => {
         if (!inited) {
@@ -65,7 +66,7 @@ function App() {
                             header={<Navbar />}
                             content={<AppRouter />}
                             sidebar={<Sidebar />}
-                            toolbar={<ScrollToolbar />}
+                            toolbar={toolbar}
                         />
                     </Suspense>
                 </div>
---
+++ src/app/lib/useAppToolbar.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from 'react';
+import { useTranslation } from 'react-i18next';
+import { AppRoutes } from '@/shared/const/router';
+import { ScrollToolbar } from '@/widgets/ScrollToolbar';
+import { useRouteChange } from '@/shared/lib/router/useRouteChange';
+
+export function useAppToolbar() {
+    const appRoute = useRouteChange();
+    const { t } = useTranslation();
+
+    const toolbarByAppRoute: OptionalRecord<AppRoutes, ReactElement> = {
+        [AppRoutes.ARTICLES]: <ScrollToolbar />,
+        [AppRoutes.ARTICLE_DETAILS]: <ScrollToolbar />,
+        [AppRoutes.MAIN]: <div>{t('EXAMPLE TOOLBAR')}</div>,
+    };
+
+    return toolbarByAppRoute[appRoute];
+}
